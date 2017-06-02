041c76fcddddcf1797a1fc018e6014332f935dd9
busyweaver <olmehdiol@gmail.com>
2017-06-02 12:53:49 +0200

    update fngen

--- fn_gen.py
+++ fn_gen.py
@@ -2,11 +2,11 @@ import math
 import random
 import sys
 import string
-noeuds=[]
+#noeuds=[]
 bino={}
 com1=1
 som_nk={}
-
+#hello moto
 def binaire(n):
     res=[]
     while n>0:
@@ -21,10 +21,10 @@ def coef(r,N,i):
     x = binomial(N-1-r, i-1)*binomial(r,i)*math.pow(2,r-i)
     return x
 
-def trouverarbre(n,N,F,to):
+def trouverarbre(n,N,F,to,noeuds):
 #    print 'N%d' % N
 #    print 'K%d' % K
-    print 'n===%d' % n
+  #  print 'n===%d' % n
     if N==0:
         print 'erreur'
         return to
@@ -35,24 +35,24 @@ def trouverarbre(n,N,F,to):
     CO=CON[N]
     re=trouverfk(n,CO)
     ri=trouverfk(int(math.ceil(re[1]/float(kf[re[0]]))),som_nk[(N,re[0])])
-    tmp=trouvernoeuds(N,re[0],int(math.ceil(re[1]/float(kf[re[0]]))),ri[0],ri[1])
-    print 'tmp = %s' % (str(tmp))
+    tmp=trouvernoeuds(N,re[0],int(math.ceil(re[1]/float(kf[re[0]]))),ri[0],ri[1],noeuds)
+    #print 'tmp = %s' % (str(tmp))
     maj_arbre(tmp,to)
 
-    trouverarbre(int(math.ceil(re[1]/float(CON[N][re[0]]/float(F[re[0]])))),re[0],F,to)          
+    trouverarbre(int(math.ceil(re[1]/float(CON[N][re[0]]/float(F[re[0]])))),re[0],F,to,noeuds)          
 
-def trouvernoeuds(N,k,ab,i,ab2):
+def trouvernoeuds(N,k,ab,i,ab2,noeuds):
     l = composition(N-k,i)
     comp=l[int(math.ceil(ab2/float(binomial(k,i)*pow(2,k-i))))-1]
-    print 'comp choisie %s' % (str(comp))
+   # print 'comp choisie %s' % (str(comp))
     #rajouter les noeuds 1
-    ra= maj_noeuds(comp,N)
-    const_arbre(N,i,k,comp)
+    ra= maj_noeuds(comp,N,k,i)
+    const_arbre(N,i,k,comp,noeuds)
     return ra
 
 
 
-def maj_noeuds(comp,N):
+def maj_noeuds(comp,N,k,i):
     #on construit un tableau selon les arites
     ra=[]
     for z in range(0,N):
@@ -60,9 +60,12 @@ def maj_noeuds(comp,N):
    
     for e in comp:
         ra[e]=ra[e]+1
+    for df in range(0,k-i):
+        lm = random.randint(0,1)
+        ra[0]=ra[0]+lm
     return ra
 
-def const_arbre(N,i,k,comp):
+def const_arbre(N,i,k,comp,noeuds):
     nodes=[]
     for bb in range(0,k):
         nodes.append(None)
@@ -200,8 +203,10 @@ def recursive_generator(N):
     elem=[]
     res=[]
     res2=[]
-    nb_ok=0
+    nb_ok=1
     ind=0
+    vb=40
+    tabtaille=[]
     for e in [0]:
         print 'nouveau'
         elem.append([])
@@ -211,33 +216,44 @@ def recursive_generator(N):
             elem[ind].append(0)
             res[ind].append(0)
             res2[ind].append(0)
-            #calcul origine de l arbre 
-
-        while nb_ok<=N*N*N:
-        
-            n=random.randint(1,kf[N])
-            #n=2173
-            print 'arbre %d' % n
-            trouverarbre(n,N,kf,elem[ind])
-            ajout_res(res[ind],elem[ind])
-   #         print 'neouds a la fin %s' % (str(noeuds))
-  #          print 'res!! %s' % (str(res[0]))
+            #calcul origine de l arbre
+      
+        taille=0
+        noeuds=[]
+        while nb_ok<=10000:
+            if(nb_ok % 100==0):
+                print '%d' % nb_ok
+            to=un_arbre(vb,elem[ind])
+           # print 'recur %s' % str(noeuds)
+           # noeuds2=recon_arbre(noeuds)
+            ajout_res(res[ind],to)
             nb_ok=nb_ok+1
             for z in range(0,len(elem[ind])):
                 elem[ind][z]=0
                 #    n=n+1
-
-            for z in range(0,len(res[ind])):
-                res2[ind][z]=res[ind][z]/float(nb_ok)
+           # taille=taille + height(noeuds2)
+            noeuds=[]
+            noeuds2=[]
+        print 'res %s ' % (str(res[ind]))
+        for z in range(0,len(res[ind])):
+            res2[ind][z]=res[ind][z]/float(nb_ok)
+        
                 
-        prob=0
-        for cv in range(2,len(res[0])):
-            prob=prob+res[0][cv]
-        print 'proba avant %d' % prob
-        print 'prob %f' % (prob/float(nb_ok))
-        nb_ok=0
-        ind=ind+1
+        # prob=0
+        # for cv in range(2,len(res[0])):
+        #     prob=prob+res[0][cv]
+        # print 'proba avant %d' % prob
+        # print 'prob %f' % (prob/float(nb_ok))
+        tabtaille.append(taille/float(nb_ok))
+        nb_ok=1
+        
+        #ind=ind+1
+        vb=vb-5
         print 'fini'
+    for yu in range(0,21):
+        print '%d %f' % (yu+1,res2[ind][yu])
+   # for fg in range(0,len(tabtaille)):
+    #    print '%d %f' % (((9-fg+1)*5),tabtaille[fg])
     return res2
 
 def ajout_res(res,elem):
@@ -246,13 +262,13 @@ def ajout_res(res,elem):
 
 
 
-def un_arbre(t,N):
+def un_arbre(t,noeuds):
     to=[]
-    for r in range(0,int(N*(N-1)*0.5)+1):
+    for r in range(0,int(t*(t-1)*0.5)+1):
         to.append(0)
     n=random.randint(1,kf[t])
-    trouverarbre(n,t,kf,to)
-    return noeuds
+    trouverarbre(n,t,kf,to,noeuds)
+    return to
 
 def gene_dot(noeuds):
     fil = open("essai.gv",'w')
@@ -359,6 +375,16 @@ def sauv_coeff():
     f.close()
 
 
+def height(arbre):
+    maxi=[]
+    if(arbre[2]==None):
+        return 0
+    for df in arbre[2]:
+        maxi.append(height(df))
+    return 1+max(maxi)
+
+
+    
 if(sys.argv[2]=='0'):
     cal_coeff()
     sauv_coeff()
@@ -367,7 +393,7 @@ elif(sys.argv[2]=='1'):
     cal_coeff()
     print 'kf %s' % str(kf)
     moy=recursive_generator(int(sys.argv[1]))
-    print 'res %s' % str(moy[0])
+#    print 'res %s' % str(moy[0])
     
 else:
     
@@ -377,7 +403,7 @@ else:
        print 'good'
        cal_coeff()
        print 'Co %s' % str(CON[5])
-       coq=un_arbre(int(sys.argv[3]),int(sys.argv[1]))
+       coq=un_arbre(int(sys.argv[3]))
        # for e in coq:
        #     print '\n'
        #     for i in e :
@@ -387,5 +413,8 @@ else:
        #             print '*,'
        print 'coq %s' % str(coq)
        noeuds2=recon_arbre(coq)
+       taille=height(noeuds2)
+       print 'taille %d' % taille
        print 'arbre %s' % str(noeuds2)
        gene_dot(noeuds2)
+
